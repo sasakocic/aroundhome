7831abf16cc1ec7c53b4754d7d6639ef1db8de90
Sasa Kocic <sasa.kocic@gmail.com>
2022-09-01 15:37:12 +0200

    route /partners delivers query

--- main.go
+++ main.go
@@ -1,24 +1,47 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	swagger "github.com/arsmn/fiber-swagger/v2"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/recover"
-	_ "github.com/sasakocic/aroundhome/docs"
+
+	_ "github.com/lib/pq"
 	"log"
 )
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "postgres"
+	dbname   = "aroundhome"
+)
+
+type Partner struct {
+	Id                 int16
+	Name               string
+	Lat                float32
+	Lng                float32
+	Radius             float32
+	Sqm                float32
+	Rating             float32
+	FlooringExperience string
+	Distance           float32
+}
 
 // @title Fiber Swagger Example API
 // @version 2.0
 // @description This is a sample server server.
 // @termsOfService http://swagger.io/terms/
 
-// @contact.name API Support
+// @contact.Name API Support
 // @contact.url http://www.swagger.io/support
 // @contact.email support@swagger.io
 
-// @license.name Apache 2.0
+// @license.Name Apache 2.0
 // @license.url http://www.apache.org/licenses/LICENSE-2.0.html
 
 // @host localhost:3000
@@ -31,15 +54,30 @@ func main() {
 	// Middleware
 	app.Use(recover.New())
 	app.Use(cors.New())
-
 	// Routes
 	app.Get("/", HealthCheck)
-	app.Get("/swagger/*", swagger.Handler) // default
+	app.Get("/swagger/*", swagger.HandlerDefault) // default
+	app.Get("/partners/*", Partners)
 
 	// Start Server
 	if err := app.Listen(":3000"); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func databaseConnect() *sql.DB {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	return db
 }
 
 // HealthCheck godoc
@@ -60,4 +98,58 @@ func HealthCheck(c *fiber.Ctx) error {
 	}
 
 	return nil
+}
+
+func Partners(c *fiber.Ctx) error {
+	db := databaseConnect()
+	defer db.Close()
+	sqlQuery := buildSql(40.076762, 113.300129)
+	//result, err := db.Exec(sqlQuery)
+	rows, err := db.Query(sqlQuery)
+	if err != nil {
+		return err
+	}
+	defer func(rows *sql.Rows) {
+		err := rows.Close()
+		if err != nil {
+
+		}
+	}(rows)
+
+	recs := make([]*Partner, 0)
+	for rows.Next() {
+		rec := new(Partner)
+		err := rows.Scan(&rec.Id, &rec.Name, &rec.Lat, &rec.Lng, &rec.Radius, &rec.Sqm, &rec.Rating, &rec.FlooringExperience, &rec.Distance)
+		//e, err := json.Marshal(rec)
+		//if err != nil {
+		//	log.Fatal(err)
+		//}
+		//log.Fatal(e)
+		if err != nil {
+			log.Fatal(err)
+		}
+		recs = append(recs, rec)
+	}
+	//e, err := json.Marshal(recs)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//fmt.Println(e)
+
+	//log.Fatal(recs)
+
+	//res := map[string]interface{}{
+	//	"test": "ok",
+	//}
+
+	if err := c.JSON(recs); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func buildSql(lat, lng float32) string {
+	return fmt.Sprintf(
+		"select\n    Id, Name, Lat, Lng, Radius, Sqm, Rating, flooring_experience AS FlooringExperience,\n    getDistance(%f, %f, Lat, Lng) AS Distance\nfrom\n    partners\nwhere\n    getDistance(40.076762, 113.300129, Lat, Lng) < Radius\norder by\n    Rating DESC,\n    Distance;", lat, lng)
 }
